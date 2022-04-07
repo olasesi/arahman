dfdb44ad6c68f0e8c4bd7d76053d30e511c19999
unknown <shobolafaruq@gmail.com>
2022-04-07 13:28:03 +0100

    Bill setting layout

--- arahman/admin/bill-setting.php
+++ arahman/admin/bill-setting.php
@@ -19,15 +19,106 @@ if($_SESSION['admin_type'] != ACCOUNTANT){
 ?>
 
 
+<?php 
+
+    mysqli_query($connect, "SELECT " )
+
+?>
+
+
 <?php require_once('../../incs-arahman/dashboard.php'); ?>
 
             <div class="main-panel">
                 <div class="content-wrapper">
+                    <div class="page-header">
+                        <h3 class="page-title"> School Fees Setting</h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 grid-margin stretch-card">
+                            <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Primary School</h4>
+                                <p class="card-description">Primary School Fees </p>
+                                <form class="forms-sample">
+                                <div class="form-group row">
+                                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Email</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputUsername2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputEmail2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputMobile" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputPassword2" class="col-sm-3 col-form-label">Password</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputPassword2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputConfirmPassword2" class="col-sm-3 col-form-label">Re Password</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputConfirmPassword2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                </form>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 grid-margin stretch-card">
+                            <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Secondary School</h4>
+                                <p class="card-description"> Secondary School Fees</p>
+                                <form class="forms-sample">
+                                <div class="form-group row">
+                                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Email</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputUsername2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputEmail2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputMobile" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputPassword2" class="col-sm-3 col-form-label">Password</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputPassword2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="exampleInputConfirmPassword2" class="col-sm-3 col-form-label">Re Password</label>
+                                    <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="exampleInputConfirmPassword2" placeholder="10,000">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                </form>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
 
                 
-
-                </div>
-            </div>
 
 
 
